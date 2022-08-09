9ebfffbcfcc4e1f954262884459fd370f306080e
koladeg <koladegureje@gmail.com>
2022-08-09 18:04:11 +0100

    start kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=koladeg/56d48c6fba1e
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run my-pod-v1 --image=$dockerpath --port=8000
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward my-pod-v1 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,16 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=koladeg/56d48c6fba1e
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag project ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
-
+# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 768752611803.dkr.ecr.us-east-1.amazonaws.com
+# docker build -t project4 .
+# docker tag project4:latest 768752611803.dkr.ecr.us-east-1.amazonaws.com/project4:latest
 # Step 3:
 # Push image to a docker repository
+# docker push 768752611803.dkr.ecr.us-east-1.amazonaws.com/project4:latest
+docker push ${dockerpath}
